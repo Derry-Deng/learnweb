7b04fd22ce5d2f5d1d797d7aedaec6d66ded5b90
DaiyuDeng <Daiyu.Deng@changhong.com>
2019-08-22 09:11:55 +0800

    加course

--- ContosoUniversity/Models/Students.cs
+++ ContosoUniversity/Models/Students.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Full Name")]
+
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstMidName;
+            }
+        }
+
+
+
         public string Secret { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
