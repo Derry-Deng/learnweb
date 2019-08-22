aeb7e55dd993ace4defdef9c614e2396b7405d36
DaiyuDeng <Daiyu.Deng@changhong.com>
2019-08-22 09:11:55 +0800

    加Deparment和并发冲突处理等等

---
+++ ContosoUniversity/Models/Department.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        //跟踪属性，用于处理并发冲突
+        [Timestamp]//指定此列包含在发送到数据库的Update和Delete命令的Where子句中
+        public byte[] RowVersion { get; set; }
+
+
+        public Instructor Administrator { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
